1774695e266b24b6f9a1a0fe1f476acb294b8739
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-05-31 13:05:08 +0200

    betand tijdelijk.py

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,6 +4,5 @@ aanbieding = [prijs * 0.8 for prijs in prijzen]
 for i in range(len(smaken_ijs)):
     print(f"{smaken_ijs[i]}: {aanbieding[i]} euro (aanbieding) of {prijzen[i]} euro (normaal)")
     Reclame_tekst = f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!"
-Reclame_tekst2 = [:0,]
-print(Reclame_tekst2)
-
+    Reclame_tekst2 = Reclame_tekst[:0]
+    print(Reclame_tekst2)
