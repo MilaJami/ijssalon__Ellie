864fa119047ca82623470c4cc0a8e24645ace2c6
MilaJami <jamila.schrijver83@gmail.com>
2025-05-31 12:31:38 +0200

    Add files via upload

---
+++ tijdelijk.py
@@ -0,0 +1,7 @@
+smaken_ijs = ['aarbei', 'vanille', 'chocola']
+prijzen = [3, 4, 5]
+aanbieding = [prijs * 0.8 for prijs in prijzen]
+for i in range(len(smaken_ijs)):
+    print(f"{smaken_ijs[i]}: {aanbieding[i]} euro (aanbieding) of {prijzen[i]} euro (normaal)")
+    Reclame_tekst = f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!"
+print(Reclame_tekst)
