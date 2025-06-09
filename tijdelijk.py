fbda7227ecf4e037cb1f949c38a239d3a7aff58a
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-06-09 21:33:04 +0200

    opdracht 1 4 toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,8 +4,6 @@ prijzen = [3, 4, 5]
 #samenvoegen
 aanbieding = [prijs * 0.8 for prijs in prijzen]
 for i in range(len(smaken_ijs)):
-    if smaken_ijs[i] == 'aardbei' or smaken_ijs[i] == 'chocola':
-        continue
- #  print (f"{smaken_ijs[i]}: {aanbieding[i]} euro (aanbieding) of {prijzen[i]} euro (normaal)")
-    reclame_tekst = (f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!")
-    print (reclame_tekst)
+    if smaken_ijs[i] == 'vanille':	 
+        reclame_tekst = (f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!")
+        print (reclame_tekst)
