830982a101f6747b9c78febba9d16560c319738b
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-06-09 21:03:18 +0200

    opdracht 1 4 toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -5,7 +5,5 @@ prijzen = [3, 4, 5]
 aanbieding = [prijs * 0.8 for prijs in prijzen]
 for i in range(len(smaken_ijs)):
  #  print (f"{smaken_ijs[i]}: {aanbieding[i]} euro (aanbieding) of {prijzen[i]} euro (normaal)")
-reclame_tekst = "Vandaag in de aanbieding: " + str(smaken_ijs[i]) + ", 1 liter slechts € " + str(aanbieding[i])
-print (f"{reclame_tekst}")
-# reclame_tekst = f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!"
-# reclame_tekst2 = "[:,0]"# reclame_tekst3 = "[:,0]"
+    reclame_tekst = (f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!")
+    print (reclame_tekst)
