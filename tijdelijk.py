94b6f66c0248ded6fc70b8bd7238100f19021f79
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-06-11 22:01:02 +0200

    opdracht 1. 5

--- tijdelijk.py
+++ tijdelijk.py
@@ -7,3 +7,8 @@ for i in range(len(smaken_ijs)):
     if smaken_ijs[i] == 'vanille':	 
         reclame_tekst = (f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!")
         print (reclame_tekst)
+        punt_index = reclame_tekst.find('.')
+        index_nul = reclame_tekst.find('0', punt_index)
+        reclame_tekst2 = reclame_tekst[:punt_index]
+        print("Volledige tekst:", reclame_tekst) 
+        print("Tot aan de 0:", reclame_tekst2)
