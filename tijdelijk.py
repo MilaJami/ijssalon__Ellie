24661ab6b702940c0045b81cacab6a5e4060329d
Jamila Schrijver <jamila.schrijver83@gmail.com>
2025-06-11 22:07:51 +0200

    opdracht 1. 6

--- tijdelijk.py
+++ tijdelijk.py
@@ -5,10 +5,10 @@ prijzen = [3, 4, 5]
 aanbieding = [prijs * 0.8 for prijs in prijzen]
 for i in range(len(smaken_ijs)):
     if smaken_ijs[i] == 'vanille':	 
-        reclame_tekst = (f"Vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!")
+        reclame_tekst = (f"vandaag in de aanbieding: {smaken_ijs[i]} 1 liter - slechts €{aanbieding[i]}!")
         print (reclame_tekst)
         punt_index = reclame_tekst.find('.')
         index_nul = reclame_tekst.find('0', punt_index)
         reclame_tekst2 = reclame_tekst[:punt_index]
-        print("Volledige tekst:", reclame_tekst) 
-        print("Tot aan de 0:", reclame_tekst2)
+        reclame_tekst3 = (reclame_tekst2.swapcase() + reclame_tekst[index_nul:])
+        print (reclame_tekst3)
